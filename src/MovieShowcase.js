656704c879553475f3aac8d7ea04e954ff93270d
Alouiscious Media <info@reneesoul.com>
2020-08-15 21:49:50 -0700

    Done.

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -5,6 +5,9 @@ import movieData from './data.js'
 export default class MovieShowcase extends Component {
 
   generateMovieCards = () => {
+    return movieData.map((movie, index) => 
+      <MovieCard item={index} title={movie.title} IMDBRating={movie.IMDBRating} genres={movie.genres} poster={movie.poster}/>
+    )
     // map over your movieData array and return an array of the correct JSX
   }
 
